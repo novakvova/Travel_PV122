f698a35327b5276655dbae63a2d340bd62b68b1a
novakvova <novakvova@gmail>
2023-07-18 18:52:47 +0300

    Add migration and update-database

---
+++ Travel/TravelApi/Migrations/20230718155155_Init database.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initdatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsDelete = table.Column<bool>(type: "boolean", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    Image = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "character varying(4000)", maxLength: 4000, nullable: false),
+                    ParentId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tblCategories_tblCategories_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "tblCategories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblVacation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Description = table.Column<string>(type: "character varying(4000)", maxLength: 4000, nullable: false),
+                    IsDelete = table.Column<bool>(type: "boolean", nullable: false),
+                    IsHot = table.Column<bool>(type: "boolean", nullable: false),
+                    DateStart = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DateFinish = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblVacation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tblVacation_tblCategories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "tblCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblVacationImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    IsDelete = table.Column<bool>(type: "boolean", nullable: false),
+                    VacationId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tblVacationImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tblVacationImage_tblVacation_VacationId",
+                        column: x => x.VacationId,
+                        principalTable: "tblVacation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblCategories_ParentId",
+                table: "tblCategories",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblVacation_CategoryId",
+                table: "tblVacation",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tblVacationImage_VacationId",
+                table: "tblVacationImage",
+                column: "VacationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblVacationImage");
+
+            migrationBuilder.DropTable(
+                name: "tblVacation");
+
+            migrationBuilder.DropTable(
+                name: "tblCategories");
+        }
+    }
+}
