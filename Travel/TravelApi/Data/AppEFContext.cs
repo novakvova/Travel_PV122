8712abc88be7230372dcee81fe9d0f62bcaf4db7
Vladyslav2309 <korolov2309@gmail.com>
2023-07-18 18:47:03 +0300

    abc

--- Travel/TravelApi/Data/AppEFContext.cs
+++ Travel/TravelApi/Data/AppEFContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Web.Providers.Entities;
 using TravelApi.Data.Entity;
 
 namespace TravelApi.Data
